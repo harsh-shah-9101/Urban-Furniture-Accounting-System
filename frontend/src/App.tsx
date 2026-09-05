e0435848624e4408994eea271056570d9dddbb6a
harsh shah <harshshah301204@gmail.com>
2026-09-05 13:04:17 +0530

    refactor(frontend): replace default React template with main application shell

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { RouterProvider } from 'react-router-dom'
+import { AppProviders } from '@/providers/AppProviders'
+import { router } from '@/router/router'
+
+function App() {
+  return (
+    <AppProviders>
+      <RouterProvider router={router} />
+    </AppProviders>
+  )
+}
+
+export default App
